2142dc313f9597bb15becc846e8e88fef3351ddf
Fuu <g.for.xyz@gmail.com>
2025-04-27 00:44:06 +0800

    tmp

--- src/defs/mod.rs
+++ src/defs/mod.rs
@@ -1,9 +1,10 @@
+mod handler_builder;
+pub mod invocation_source;
 pub mod layer;
 
-mod handler_builder;
-pub use handler_builder::HandlerBuilder;
+use std::marker::PhantomData;
 
-pub mod invocation_source;
+pub use self::handler_builder::HandlerBuilder;
 
 #[derive(Debug)]
 pub struct Invocation<Arg, Cb> {
@@ -14,21 +15,26 @@ pub struct Invocation<Arg, Cb> {
 pub trait Handler<Arg> {
     type Ret;
 
-    fn handle(&mut self, arg: Arg, callback: impl Callback<Self::Ret>);
+    fn handle(&mut self, arg: Arg, callback: impl Callback<Output = Self::Ret>);
 }
 
-pub trait Callback<T>: Send + 'static {
-    fn call(self, val: T);
+pub trait Callback: Send + 'static {
+    type Output;
+
+    fn call(self, out: Self::Output);
 }
 
 pub trait TryHandler<Arg>: Handler<Arg, Ret = Result<Self::Ok, Self::Err>> {
     type Ok;
     type Err;
 
-    fn handle(&mut self, arg: Arg, callback: impl TryCallback<Self::Ok, Self::Err>);
+    fn handle(&mut self, arg: Arg, callback: impl TryCallback<Ok = Self::Ok, Err = Self::Err>);
 }
 
-pub trait TryCallback<T, E>: Callback<Result<T, E>> {}
+pub trait TryCallback: Callback<Output = Result<Self::Ok, Self::Err>> {
+    type Ok;
+    type Err;
+}
 
 impl<H, Arg, Ok, Err> TryHandler<Arg> for H
 where
@@ -37,18 +43,42 @@ where
     type Ok = Ok;
     type Err = Err;
 
-    fn handle(&mut self, arg: Arg, callback: impl TryCallback<Self::Ok, Self::Err>) {
+    fn handle(&mut self, arg: Arg, callback: impl TryCallback<Ok = Self::Ok, Err = Self::Err>) {
         self.handle(arg, callback)
     }
 }
 
-impl<Cb, Ok, Err> TryCallback<Ok, Err> for Cb where Cb: Callback<Result<Ok, Err>> {}
-
-impl<T, F> Callback<T> for F
+impl<Cb, Ok, Err> TryCallback for Cb
 where
-    F: FnOnce(T) + Send + 'static,
+    Cb: Callback<Output = Result<Ok, Err>>,
 {
-    fn call(self, val: T) {
-        self(val)
+    type Ok = Ok;
+    type Err = Err;
+}
+
+struct FnCallback<F, Output> {
+    f: F,
+    _phantom: PhantomData<fn(Output)>,
+}
+
+impl<F, Output> Callback for FnCallback<F, Output>
+where
+    F: FnOnce(Output) + Send + 'static,
+    Output: 'static,
+{
+    type Output = Output;
+
+    fn call(self, out: Self::Output) {
+        (self.f)(out);
+    }
+}
+
+fn callback_fn<F, Output>(f: F) -> FnCallback<F, Output>
+where
+    F: FnOnce(Output) + Send + 'static,
+{
+    FnCallback {
+        f,
+        _phantom: Default::default(),
     }
 }
